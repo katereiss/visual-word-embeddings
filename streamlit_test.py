48dd3b2cd3a9789f6c43fa2dde3ef467d6947b57
katereiss <84412675+katereiss@users.noreply.github.com>
2022-04-07 18:28:34 -0400

    update .py

--- streamlit_test.py
+++ streamlit_test.py
@@ -12,16 +12,9 @@ from sklearn.decomposition import PCA
 st.title('Visualizing Word Embeddings')
 
 model_wikipedia50 = api.load("glove-wiki-gigaword-50")
-# model_twitter25 = api.load("glove-twitter-25")
-
-# st.selectbox('Select', ['GloVe Wikipedia 50 dimensions','GloVe Twitter 25 dimensions'])
-
-# Create a text element and let the reader know the data is loading.
-# data_load_state = st.text('Loading data...')
-# Load pre-trained embeddings
+#
 data = model_wikipedia50
-# Notify the reader that the data was successfully loaded.
-# data_load_state.text('Loading data...done!')
+
 
 st.header('What Are Word Embeddings?')
 
@@ -82,7 +75,7 @@ try:
 except KeyError:
     st.write('Sorry! \"',title,"\" not in vocabulary." )
     
-@st.cache
+@st.cache(suppress_st_warning=True)
 def display_pca_scatterplot_3D(model=model_wikipedia50, user_input=None, words=None, label=None, color_map=None, topn=5, sample=10):
 
     if words == None:
@@ -95,8 +88,6 @@ def display_pca_scatterplot_3D(model=model_wikipedia50, user_input=None, words=N
     
     
     three_dim = PCA(random_state=0).fit_transform(word_vectors)[:,:3]
-    # For 2D, change the three_dim variable into something like two_dim like the following:
-    # two_dim = PCA(random_state=0).fit_transform(word_vectors)[:,:2]
 
     data = []
     count = 0
@@ -118,9 +109,7 @@ def display_pca_scatterplot_3D(model=model_wikipedia50, user_input=None, words=N
         }
 
                 )
-                
-        # For 2D, instead of using go.Scatter3d, we need to use go.Scatter and delete the z variable. Also, instead of using
-        # variable three_dim, use the variable that we have declared earlier (e.g two_dim)
+
 
         data.append(trace)
         count = count+topn
@@ -141,12 +130,8 @@ def display_pca_scatterplot_3D(model=model_wikipedia50, user_input=None, words=N
                     }
                     )
 
-    # For 2D, instead of using go.Scatter3d, we need to use go.Scatter and delete the z variable.  Also, instead of using
-    # variable three_dim, use the variable that we have declared earlier (e.g two_dim)
             
     data.append(trace_input)
-    
-# Configure the layout
 
 
     layout = go.Layout(
